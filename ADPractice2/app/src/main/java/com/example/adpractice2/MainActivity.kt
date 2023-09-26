710ffcf877d794af3b33b799bfee137cf7a6a45f
Asier_Garcia _PCCasa <asgal.gl@gmail.com>
2023-09-26 23:24:37 +0200

    crud acabado

--- ADPractice2/app/src/main/java/com/example/adpractice2/MainActivity.kt
+++ ADPractice2/app/src/main/java/com/example/adpractice2/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.adpractice2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.height
+import android.R.attr.width
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val db = Firebase.firestore
-        val texto_id = findViewById<EditText>(R.id.text_id)
-        val texto_descripcion = findViewById<EditText>(R.id.text_descripcion)
-        val texto_precio = findViewById<EditText>(R.id.text_precio)
+        val texto_id = findViewById<EditText>(R.id.text_id).text
+        val texto_descripcion = findViewById<EditText>(R.id.text_descripcion).text
+        val texto_precio = findViewById<EditText>(R.id.text_precio).text
 
         val boton_alta = findViewById<Button>(R.id.boton_alta)
         val boton_baja = findViewById<Button>(R.id.boton_baja)
@@ -33,13 +34,13 @@ class MainActivity : AppCompatActivity() {
         var productos = ""
 
         fun getProducts() {
+            lista.text = ""
+            productos = ""
             db.collection("productos")
                 .get()
                 .addOnSuccessListener { result ->
                     for (document in result) {
-                        productos =
-                            productos + "\n" + "Producto: " + document.data.get("descripcion")
-                                .toString() + " Precio: " + document.data.get("precio").toString()
+                        productos = productos + "\n" + "Producto: " + document.data.get("descripcion").toString() + " Precio: " + document.data.get("precio").toString()
                         lista.text = productos
                     }
                 }
@@ -59,8 +60,8 @@ class MainActivity : AppCompatActivity() {
                 .get()
                 .addOnSuccessListener { result ->
                     for (document in result){
-                        if(document.data.get("id") == texto_id.text.toString().toLong()) {
-                            productos = "Producto: " + document.data.get("descripcion").toString() + " Precio: " + document.data.get("precio").toString()
+                        if(document.data.get("id") == texto_id.toString().toLong()) {
+                            productos = "Producto: " + document.data.get("descripcion").toString() + " ...... Precio: " + document.data.get("precio").toString()
                             lista.text = productos
                         }
                     }
@@ -72,7 +73,7 @@ class MainActivity : AppCompatActivity() {
                 .get()
                 .addOnSuccessListener { result ->
                     for (document in result){
-                        if(document.data.get("descripcion").toString().toLowerCase() == texto_descripcion.text.toString().toLowerCase()) {
+                        if(document.data.get("descripcion").toString().toLowerCase() == texto_descripcion.toString().toLowerCase()) {
                             productos = "Producto: " + document.data.get("descripcion").toString() + " Precio: " + document.data.get("precio").toString()
                             lista.text = productos
                         }
@@ -83,20 +84,73 @@ class MainActivity : AppCompatActivity() {
 
         boton_alta.setOnClickListener {
             var newProduct = hashMapOf(
-                "id" to texto_id.text,
-                "descripcion" to texto_descripcion.text.toString(),
-                "precio" to texto_precio.text
+                "id" to texto_id.toString().toInt(),
+                "descripcion" to texto_descripcion.toString(),
+                "precio" to texto_precio.toString().toInt()
             )
 
-            db.collection("productos").document("$texto_id")
+            db.collection("productos").document("${texto_id.toString().toInt()}")
                 .set(newProduct)
                 .addOnCompleteListener {
                     Toast.makeText(this, "Producto subido", Toast.LENGTH_LONG).show()
+                    getProducts()
                 }
                 .addOnFailureListener {
                     Toast.makeText(this, "Error al subir", Toast.LENGTH_LONG).show()
                 }
 
         }
+
+        boton_baja.setOnClickListener{
+            db.collection("productos").document("${texto_id.toString().toInt()}")
+                .delete()
+                .addOnCompleteListener {
+                    Toast.makeText(this, "Producto borrado", Toast.LENGTH_LONG).show()
+                    getProducts()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error al borrar", Toast.LENGTH_LONG).show()
+                }
+        }
+
+        boton_modificar.setOnClickListener{
+            var newProduct = hashMapOf(
+                "id" to texto_id.toString().toInt(),
+                "descripcion" to texto_descripcion.toString(),
+                "precio" to texto_precio.toString().toInt()
+            )
+
+            db.collection("productos").document("${texto_id.toString().toInt()}")
+                .update("descripcion", texto_descripcion.toString(),
+                    "precio", texto_precio.toString().toInt())
+                .addOnCompleteListener {
+                    Toast.makeText(this, "Producto actualizado", Toast.LENGTH_LONG).show()
+                    getProducts()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error al actualizar", Toast.LENGTH_LONG).show()
+                }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
